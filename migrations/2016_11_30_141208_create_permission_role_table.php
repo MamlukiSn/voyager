d8161b9e1e77955dd0a26498a8ab6c001ca77513
Kenneth  Kariuki <kenneth.kariuki@squaddigital.com>
2019-04-11 15:19:50 +0300

    fixed foreign key references

--- migrations/2016_11_30_141208_create_permission_role_table.php
+++ migrations/2016_11_30_141208_create_permission_role_table.php
@@ -13,11 +13,11 @@ class CreatePermissionRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('lvpermission_role', function (Blueprint $table) {
+        Schema::create('permission_role', function (Blueprint $table) {
             $table->integer('permission_id')->unsigned()->index();
-            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('lvpermissions')->onDelete('cascade');
             $table->integer('role_id')->unsigned()->index();
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('lvroles')->onDelete('cascade');
             $table->primary(['permission_id', 'role_id']);
         });
     }
@@ -29,6 +29,6 @@ class CreatePermissionRoleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lvpermission_role');
+        Schema::dropIfExists('permission_role');
     }
 }
