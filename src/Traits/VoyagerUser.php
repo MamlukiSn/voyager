7fa6b58162724908850f3edc46896577d18d3f84
Kenneth  Kariuki <kenneth.kariuki@squaddigital.com>
2019-04-24 12:08:25 +0300

    add konect traits to user

--- src/Traits/VoyagerUser.php
+++ src/Traits/VoyagerUser.php
@@ -4,12 +4,22 @@ namespace TCG\Voyager\Traits;
 
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use TCG\Voyager\Facades\Voyager;
+use Illuminate\Support\Collection;
+use Konekt\Acl\Contracts\Role;
+use Illuminate\Database\Eloquent\Builder;
+use Konekt\Acl\Contracts\Permission;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Konekt\Acl\Models\PermissionProxy;
+use Konekt\Acl\Models\RoleProxy;
+use Konekt\Acl\Traits\HasPermissions;
 
 /**
  * @property  \Illuminate\Database\Eloquent\Collection  roles
  */
 trait VoyagerUser
 {
+    use HasPermissions;
+
     /**
      * Return default User Role.
      */
@@ -24,6 +34,20 @@ trait VoyagerUser
     public function roles()
     {
         return $this->belongsToMany(Voyager::modelClass('Role'), 'user_roles');
+    }
+
+    /**
+     * Return alternative User Roles.
+     */
+    public function rolesKonect() : MorphToMany
+    {
+        return $this->morphToMany(
+            RoleProxy::modelClass(),
+            'model',
+            'model_roles',
+            'model_id',
+            'role_id'
+        );
     }
 
     /**
@@ -43,7 +67,7 @@ trait VoyagerUser
      *
      * @return bool
      */
-    public function hasRole($name)
+    public function hasRole($name): bool
     {
         $roles = $this->roles_all()->pluck('name')->toArray();
 
@@ -53,8 +77,43 @@ trait VoyagerUser
             }
         }
 
-        return false;
+        return $this->hasRoleKonect($name);
     }
+
+    /**
+     * Determine if the model has (one of) the given role(s).
+     *
+     * @param string|array|\Konekt\Acl\Contracts\Role|\Illuminate\Support\Collection $roles
+     *
+     * @return bool
+     */
+    public function hasRoleKonect($roles): bool
+    {
+        if (is_string($roles) && false !== strpos($roles, '|')) {
+            $roles = $this->convertPipeToArray($roles);
+        }
+
+        if (is_string($roles)) {
+            return $this->rolesKonect->contains('name', $roles);
+        }
+
+        if ($roles instanceof Role) {
+            return $this->rolesKonect->contains('id', $roles->id);
+        }
+
+        if (is_array($roles)) {
+            foreach ($roles as $role) {
+                if ($this->hasRoleKonect($role)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return $roles->intersect($this->rolesKonect)->isNotEmpty();
+    }
+
 
     /**
      * Set default User Role.
@@ -121,5 +180,347 @@ trait VoyagerUser
             $this->role->load('permissions');
             $this->load('roles.permissions');
         }
+    }
+
+    /**
+     * A model may have multiple direct permissions.
+     */
+    public function permissions(): MorphToMany
+    {
+        return $this->morphToMany(
+            PermissionProxy::modelClass(),
+            'model',
+            'model_permissions',
+            'model_id',
+            'permission_id'
+        );
+    }
+
+    /**
+     * Scope the model query to certain roles only.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string|array|Role|\Illuminate\Support\Collection $roles
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeRole(Builder $query, $roles): Builder
+    {
+        if ($roles instanceof Collection) {
+            $roles = $roles->all();
+        }
+
+        if (! is_array($roles)) {
+            $roles = [$roles];
+        }
+
+        $roles = array_map(function ($role) {
+            if ($role instanceof Role) {
+                return $role;
+            }
+
+            return RoleProxy::findByName($role, $this->getDefaultGuardName());
+        }, $roles);
+
+        return $query->whereHas('roles', function ($query) use ($roles) {
+            $query->where(function ($query) use ($roles) {
+                foreach ($roles as $role) {
+                    $query->orWhere('roles.id', $role->id);
+                }
+            });
+        });
+    }
+
+    /**
+     * @param string|array|\Spatie\Permission\Contracts\Permission|\Illuminate\Support\Collection $permissions
+     *
+     * @return array
+     */
+    protected function convertToPermissionModels($permissions): array
+    {
+        if ($permissions instanceof Collection) {
+            $permissions = $permissions->all();
+        }
+
+        $permissions = array_wrap($permissions);
+
+        return array_map(function ($permission) {
+            if ($permission instanceof Permission) {
+                return $permission;
+            }
+
+            return app(Permission::class)->findByName($permission, $this->getDefaultGuardName());
+        }, $permissions);
+    }
+
+    /**
+     * Scope the model query to certain permissions only.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param string|array|\Konekt\Acl\Contracts\Permission|\Illuminate\Support\Collection $permissions
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePermission(Builder $query, $permissions): Builder
+    {
+        $permissions = $this->convertToPermissionModels($permissions);
+
+        $rolesWithPermissions = array_unique(array_reduce($permissions, function ($result, $permission) {
+            return array_merge($result, $permission->rolesKonect->all());
+        }, []));
+
+        return $query->
+        where(function ($query) use ($permissions, $rolesWithPermissions) {
+            $query->whereHas('permissions', function ($query) use ($permissions) {
+                $query->where(function ($query) use ($permissions) {
+                    foreach ($permissions as $permission) {
+                        $query->orWhere('permissions.id', $permission->id);
+                    }
+                });
+            });
+            if (count($rolesWithPermissions) > 0) {
+                $query->orWhereHas('roles', function ($query) use ($rolesWithPermissions) {
+                    $query->where(function ($query) use ($rolesWithPermissions) {
+                        foreach ($rolesWithPermissions as $role) {
+                            $query->orWhere('roles.id', $role->id);
+                        }
+                    });
+                });
+            }
+        });
+    }
+
+    /**
+     * Assign the given role to the model.
+     *
+     * @param array|string|\Konekt\Acl\Contracts\Role ...$roles
+     *
+     * @return $this
+     */
+    public function assignRole(...$roles)
+    {
+        $roles = collect($roles)
+            ->flatten()
+            ->map(function ($role) {
+                return $this->getStoredRole($role);
+            })
+            ->each(function ($role) {
+                $this->ensureModelSharesGuard($role);
+            })
+            ->all();
+
+        $this->rolesKonect()->saveMany($roles);
+
+        $this->forgetCachedPermissions();
+
+        return $this;
+    }
+
+    /**
+     * Revoke the given role from the model.
+     *
+     * @param string|\Konekt\Acl\Contracts\Role $role
+     */
+    public function removeRole($role)
+    {
+        $this->rolesKonect()->detach($this->getStoredRole($role));
+    }
+
+    /**
+     * Remove all current roles and set the given ones.
+     *
+     * @param array|Role|string ...$roles
+     *
+     * @return $this
+     */
+    public function syncRoles(...$roles)
+    {
+        $this->rolesKonect()->detach();
+
+        return $this->assignRole($roles);
+    }
+
+    /**
+     * Determine if the model has any of the given role(s).
+     *
+     * @param string|array|\Konekt\Acl\Contracts\Role|\Illuminate\Support\Collection $roles
+     *
+     * @return bool
+     */
+    public function hasAnyRole($roles): bool
+    {
+        return $this->hasRole($roles);
+    }
+
+    /**
+     * Determine if the model has all of the given role(s).
+     *
+     * @param string|array|\Konekt\Acl\Contracts\Role|\Illuminate\Support\Collection $roles
+     *
+     * @return bool
+     */
+    public function hasAllRoles($roles): bool
+    {
+        if (is_string($roles) && false !== strpos($roles, '|')) {
+            $roles = $this->convertPipeToArray($roles);
+        }
+
+        if (is_string($roles)) {
+            return $this->rolesKonect->contains('name', $roles);
+        }
+
+        if ($roles instanceof Role) {
+            return $this->rolesKonect->contains('id', $roles->id);
+        }
+
+        $roles = collect()->make($roles)->map(function ($role) {
+            return $role instanceof Role ? $role->name : $role;
+        });
+
+        return $roles->intersect($this->rolesKonect->pluck('name')) == $roles;
+    }
+
+    /**
+     * Determine if the model may perform the given permission.
+     *
+     * @param string|\Konekt\Acl\Contracts\Permission $permission
+     * @param string|null $guardName
+     *
+     * @return bool
+     */
+    public function hasPermissionTo($permission, $guardName = null): bool
+    {
+        if (is_string($permission)) {
+            $permission = PermissionProxy::findByName(
+                $permission,
+                $guardName ?? $this->getDefaultGuardName()
+            );
+        }
+
+        return $this->hasDirectPermission($permission) || $this->hasPermissionViaRole($permission);
+    }
+
+    /**
+     * Determine if the model has any of the given permissions.
+     *
+     * @param array ...$permissions
+     *
+     * @return bool
+     */
+    public function hasAnyPermission(...$permissions): bool
+    {
+        if (is_array($permissions[0])) {
+            $permissions = $permissions[0];
+        }
+
+        foreach ($permissions as $permission) {
+            if ($this->hasPermissionTo($permission)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the model has, via roles, the given permission.
+     *
+     * @param \Konekt\Acl\Contracts\Permission $permission
+     *
+     * @return bool
+     */
+    protected function hasPermissionViaRole(Permission $permission): bool
+    {
+        return $this->hasRole($permission->roles);
+    }
+
+    /**
+     * Determine if the model has the given permission.
+     *
+     * @param string|\Konekt\Acl\Contracts\Permission $permission
+     *
+     * @return bool
+     */
+    public function hasDirectPermission($permission): bool
+    {
+        if (is_string($permission)) {
+            $permission = PermissionProxy::findByName($permission, $this->getDefaultGuardName());
+
+            if (! $permission) {
+                return false;
+            }
+        }
+
+        return $this->permissions->contains('id', $permission->id);
+    }
+
+    /**
+     * Return all permissions the directory coupled to the model.
+     */
+    public function getDirectPermissions(): Collection
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * Return all the permissions the model has via roles.
+     */
+    public function getPermissionsViaRoles(): Collection
+    {
+        return $this->load('roles', 'roles.permissions')
+            ->rolesKonect->flatMap(function ($role) {
+                return $role->permissions;
+            })->sort()->values();
+    }
+
+    /**
+     * Return all the permissions the model has, both directly and via roles.
+     */
+    public function getAllPermissions(): Collection
+    {
+        return $this->permissions
+            ->merge($this->getPermissionsViaRoles())
+            ->sort()
+            ->values();
+    }
+
+    public function getRoleNames(): Collection
+    {
+        return $this->rolesKonect->pluck('name');
+    }
+
+    protected function getStoredRole($role): Role
+    {
+        if (is_numeric($role)) {
+            return RoleProxy::findById($role, $this->getDefaultGuardName());
+        }
+
+        if (is_string($role)) {
+            return RoleProxy::findByName($role, $this->getDefaultGuardName());
+        }
+
+        return $role;
+    }
+
+    protected function convertPipeToArray(string $pipeString)
+    {
+        $pipeString = trim($pipeString);
+
+        if (strlen($pipeString) <= 2) {
+            return $pipeString;
+        }
+
+        $quoteCharacter = substr($pipeString, 0, 1);
+        $endCharacter   = substr($quoteCharacter, -1, 1);
+
+        if ($quoteCharacter !== $endCharacter) {
+            return explode('|', $pipeString);
+        }
+
+        if (! in_array($quoteCharacter, ["'", '"'])) {
+            return explode('|', $pipeString);
+        }
+
+        return explode('|', trim($pipeString, $quoteCharacter));
     }
 }
